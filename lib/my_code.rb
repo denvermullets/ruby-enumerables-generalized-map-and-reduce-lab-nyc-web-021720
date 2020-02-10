79a80be88091f3306e8670c6c2584b1be8543894
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-10 23:25:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
 # Your Code Here
+
+def map (array)
+  
+  new_array = []
+  index = 0 
+  while index < array.length do 
+    new.push(yield(array[index]))
+    index += 1 
+  end 
+  
+  new_array
+  
+  
+end
