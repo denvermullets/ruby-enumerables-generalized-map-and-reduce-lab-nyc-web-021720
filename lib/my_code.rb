cbae807060042cbb6326a44d93905556ff3fb43e
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-10 23:28:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,7 +5,7 @@ def map(array)
   new_array = []
   index = 0 
   while index < array.length do 
-    new.push(yield(array[index]))
+    new_array.push(yield(array[index]))
     index += 1 
   end 
   
